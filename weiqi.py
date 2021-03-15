84d748b8888d76b3aca631231d226d078c7c3aaa
c-j-lh <chieuleheng@gmail.com>
2021-03-15 21:55:04 +0800

    Lab 11 submission

--- weiqi.py
+++ weiqi.py
@@ -23,29 +23,34 @@ def login():
     # Output message if something goes wrong...
     msg = ''
     
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    # Check if "name" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form:
         # Create variables for easy access
-        username = request.form['username']
+        name = request.form['name']
         password = request.form['password']
+        if not re.match(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', password):
+            msg = 'Invalid DOB!'
+        else:
         
     # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM player WHERE name = %s AND dob = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        
-        # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['username'] = account['name']
-            # Redirect to home page
-            return redirect(url_for('home'))
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    return render_template('index.html', msg='')
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM player WHERE name = %s AND dob = %s', (name, password,))
+            print(f'SELECT * FROM player WHERE name = {name} AND dob = {password}')
+            # Fetch one record and return result
+            account = cursor.fetchone()
+            print('account:',account)
+            
+            # If account exists in accounts table in out database
+            if account:
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['name'] = account['name']
+                # Redirect to home page
+                return redirect(url_for('home'))
+            else:
+                # Account doesnt exist or name/password incorrect
+                msg = 'Incorrect name/password!'
+    return render_template('index.html', msg=msg)
     
 # http://localhost:5000/python/logout - this will be the logout page
 @app.route('/pythonlogin/logout')
@@ -53,7 +58,7 @@ def logout():
     # Remove session data, this will log the user out
    session.pop('loggedin', None)
    #session.pop('id', None)
-   session.pop('username', None)
+   session.pop('name', None)
    # Redirect to login page
    return redirect(url_for('login'))
 
@@ -62,28 +67,33 @@ def logout():
 def register():
     # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    # Check if "name", "password" and "ranking" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'ranking' in request.form:
         # Create variables for easy access
-        username = request.form['username']
+        name = request.form['name']
         password = request.form['password']
-        email = request.form['email']
+        ranking = request.form['ranking']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM player WHERE name = %s', (username,))
+        cursor.execute('SELECT * FROM player WHERE name = %s', (name,))
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
             msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
+        elif not re.match(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', password):
+            print('\n\npassword:',password, '\n\n')
+            msg = 'Invalid DOB!'
+        elif not re.match(r'[0-9]{2}[kdps]', ranking):
+            msg = 'Invalid ranking!'
+        #elif not re.match(r'[^@]+@[^@]+\.[^@]+', ranking):
+        #    msg = 'Invalid ranking!'
+        elif not re.match(r'[A-Za-z0-9]+', name):
+            msg = 'name must contain only characters and numbers!'
+        elif not name or not password or not ranking:
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO player VALUES (%s, NULL, %s, false, NULL, NULL, NULL)', (username, password, ))
+            cursor.execute('INSERT INTO player VALUES (%s, %s, %s, false, NULL, NULL, NULL)', (name, ranking, password, ))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
@@ -104,7 +114,7 @@ def home():
         data = cursor.fetchall()
         print('cursor:', data)
         # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'], data=data)
+        return render_template('home.html', name=session['name'], data=data)
        
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
