a4b8306e5f874439e5e01ef096b7612075b39341
c-j-lh <chieuleheng@gmail.com>
2021-03-17 18:41:24 +0800

    ddl sample done

--- weiqi.sql
+++ weiqi.sql
@@ -2,6 +2,12 @@ drop database if exists weiqi;
 create database weiqi;
 use weiqi;
 
+create table Country(
+	name	varchar(100),
+    flag	varchar(1000),  -- image url
+	primary key (name)
+);
+
 create table Player(
   name         varchar(100) not null,
   ranking      varchar(5),
@@ -10,21 +16,58 @@ create table Player(
   coder	   	   decimal(10,2),
   info         text,
   algoType     varchar(100),
+  countryName  varchar(100),
+  foreign key(countryName) references Country(name),
   primary key (name)
-);
-
-create table Country(
-	name	varchar(100),
-    flag	varchar(1000),  -- image url
-	primary key (name)
 );
 
 create table Game(
 		id	int,
         result	varchar(10),
+        startDate date,
+        countryName varchar(100),
         playerNameBlack varchar(100) not null,
         playerNameWhite varchar(100) not null,
         primary key(id),
         foreign key(playerNameBlack) references Player(name),
-        foreign key(playerNameWhite) references Player(name)
+        foreign key(playerNameWhite) references Player(name),
+        foreign key(countryName) references Country(name)
 );
+
+create table Move(
+	gameID int,
+    moveNo int,
+    positionX int, positionY int,
+    primary key(gameID, moveNo),
+    foreign key(gameID) references Game(id)
+);
+
+create table Comment(
+	id int,
+    text_ text,
+    voteCount int,
+    playerNameTyped varchar(100),
+    gameIDAbout int,
+    moveNoAbout int,
+    primary key(id),
+    foreign key(playerNameTyped) references Player(name),
+    foreign key(gameIDAbout, moveNoAbout) references Move(gameID, moveNo)
+);
+
+create table Votes(
+	playerName varchar(100),
+	commentID int,
+    downUp boolean,  -- False for down, True for up
+    primary key(playerNAme, commentID),
+    foreign key(playerName) references Player(name),
+    foreign key(commentID) references Comment(ID)
+);
+
+create table Tags(
+	playerName varchar(100),
+	commentID int,
+    primary key(playerName, commentID),
+    foreign key(playerName) references Player(name),
+    foreign key(commentID) references Comment(ID)
+);
+
--- weiqi_data.sql
+++ weiqi_data.sql
@@ -1,14 +1,14 @@
 use weiqi;
 
 delete from player;
-INSERT INTO player VALUES ("Le Heng","1s","2003-12-02",false, null, null, null);
-INSERT INTO player VALUES ("Le Heng2","1s","2003-12-02",false, null, null, null);
-INSERT INTO player VALUES ("Le Heng3","1s","2003-12-02",false, null, null, null);
-INSERT INTO player VALUES ("Le Heng4","1s","2003-12-02",false, null, null, null);
-INSERT INTO player VALUES ("Le Heng5","1s","2003-12-02",false, null, null, null);
-INSERT INTO player VALUES ("Le Heng6","1s","2003-12-02",false, null, null, null);
-INSERT INTO player VALUES ("Le Heng7","1s","2003-12-02",false, null, null, null);
-INSERT INTO player VALUES ("Le Heng8","1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng","Korea", "1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng2","Korea", "1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng3","Korea", "1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng4","Korea", "1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng5","Korea", "1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng6","Korea", "1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng7","Korea", "1s","2003-12-02",false, null, null, null);
+INSERT INTO player VALUES ("Le Heng8","Korea", "1s","2003-12-02",false, null, null, null);
 select * from player;
 
 delete from country;
@@ -18,4 +18,14 @@ INSERT INTO country VALUES ("Korea","China.jpg");
 select * from country;
 
 delete from game;
-INSERT INTO game VALUES (0,"B+7.5", "Le Heng2", "Le Heng3");
+INSERT INTO game VALUES (0,"B+7.5", "2020-10-05", NULL, "Le Heng2", "Le Heng3");
+
+delete from Move;
+INSERT INTO move VALUES (0,0, 3,4);
+INSERT INTO move VALUES (0,1, 17,4);
+INSERT INTO move VALUES (0,2, NULL, NULL);
+
+insert into comment values (0, "Good move", -2, "Le Heng2", 0, 2);
+
+insert into Votes values ("Le Heng1", 0, False), ("Le Heng3", 0, False);
+insert into Tags values ("Le Heng1", 0);
